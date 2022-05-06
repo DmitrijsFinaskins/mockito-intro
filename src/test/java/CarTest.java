5ffa6e602dce374032d6b4ed415504a56fd76af9
DmitrijsF <dmitrijs@jugla.net>
2022-05-06 19:32:20 +0300

    lesson2 - Mockito when

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -3,6 +3,7 @@ import org.mockito.Mockito;
 import org.junit.jupiter.api.Assertions.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 public class CarTest {
 
@@ -12,5 +13,19 @@ public class CarTest {
         Car car = new Car("",  "", 2014, "");
         Car newCar = Mockito.mock(Car.class);
         assertEquals(0, newCar.getYear());
+    }
+
+    @Test
+    void remoteServissReturnValue(){
+        Car newCar = Mockito.mock(Car.class);
+        when(newCar.testInt(4)).thenReturn(10);
+        assertEquals(10, newCar.testInt(4));
+    }
+
+    @Test
+    void getOwner(){
+        Car newCar = Mockito.mock(Car.class);
+        when(newCar.getOwner()).thenReturn("Dmitrij");
+        assertEquals("Dmitrij", newCar.getOwner());
     }
 }
