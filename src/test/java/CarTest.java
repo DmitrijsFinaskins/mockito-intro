ed2f9d50462a3bdbd033c91ce0fa292bd288138e
DmitrijsF <dmitrijs@jugla.net>
2022-07-04 10:37:17 +0300

    lesson4 - Mockito verify additional options, @Mock annotation

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,11 +1,22 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.junit.jupiter.api.Assertions.*;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class CarTest {
+
+    @Mock
+    Car newCar;
+
+    @BeforeEach
+    void setup(){
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     void createCar(){
@@ -17,22 +28,19 @@ public class CarTest {
 
     @Test
     void remoteServissReturnValue(){
-        Car newCar = Mockito.mock(Car.class);
         when(newCar.testInt(4)).thenReturn(10);
         assertEquals(10, newCar.testInt(4));
     }
 
     @Test
     void getOwner(){
-        Car newCar = Mockito.mock(Car.class);
         when(newCar.getOwner()).thenReturn("Dmitrij");
         assertEquals("Dmitrij", newCar.getOwner());
     }
 
     @Test
     void verificationTest(){
-        Car newCar = Mockito.mock(Car.class);
         assertEquals(null, newCar.getOwner());
-        verify(newCar).getOwner();
+        verify(newCar, only()).getOwner();
     }
 }
